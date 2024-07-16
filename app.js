151df9f376b9a53e0c79ef312f83b2196d8a4a7e
Gustavo S. <44175853+gsm1911@users.noreply.github.com>
2024-07-15 22:00:51 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,47 @@
+let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
+function exibirTextoNaTela(tag, texto){
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+function verificarChute(){
+    let chute = document.querySelector('input').value;
+    console.log(`${numeroSecreto}`);
+    let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+    let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`
+    if (chute == numeroSecreto){
+        exibirTextoNaTela('h1', 'Acertou!');
+        exibirTextoNaTela('p', mensagemTentativas);
+        botaoChutar = document.getElementById('chutar');
+        botaoChutar.disabled = true;
+        botaoChutar = document.getElementById('reiniciar');
+        botaoChutar.disabled = false;
+    }
+    else{
+        exibirTextoNaTela('h1', 'Errou...');
+        if(chute < numeroSecreto){
+            exibirTextoNaTela('p', 'Chutou baixo...');
+        }else{
+            exibirTextoNaTela('p', 'Chutou alto...');
+        }
+        campo = document.querySelector('input');
+        campo.value = ' ';
+        tentativas++;
+    }
+}
+
+function gerarNumeroAleatorio(){
+    return parseInt(Math.random() * 10 + 1);
+}
+
+function novoJogo(){
+    numeroSecreto = gerarNumeroAleatorio();
+    exibirTextoNaTela('h1', 'Jogo do número secreto');
+    exibirTextoNaTela('p', 'Escolha um número de 1 a 10');
+    botaoChutar = document.getElementById('chutar');
+    botaoChutar.disabled = false;
+}
+
+exibirTextoNaTela('h1', 'Jogo do número secreto');
+exibirTextoNaTela('p', 'Escolha um número de 1 a 10');
